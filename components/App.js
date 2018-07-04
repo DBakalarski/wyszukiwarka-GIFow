3d28b6dd3d57bdd9e746eb8bbcbe146859b3d093
Dawid Bakalarski <dawid.bakalarski@gmail.com>
2018-07-04 15:45:06 +0200

    Add promise

--- components/App.js
+++ components/App.js
@@ -15,30 +15,39 @@ App = React.createClass({
 	    this.setState({
 	      loading: true 
 	    });
-	    this.getGif(searchingText, function(gif) { 
+	    this.getGif(searchingText)
+	    	.then(function(gif) { 
 	      this.setState({  
 	        loading: false, 
 	        gif: gif, 
 	        searchingText: searchingText  
 	      });
 	    }.bind(this));
+	    .catch(function(error) {
+	    	console.log('Error')
+	    });
 	  },
 
-	  getGif: function(searchingText, callback) {  
-	    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
-	    var xhr = new XMLHttpRequest(); 
-	    xhr.open('GET', url);
-	    xhr.onload = function() {
-	        if (xhr.status === 200) {
-	           var data = JSON.parse(xhr.responseText).data; 
-	            var gif = {  
-	                url: data.fixed_width_downsampled_url,
-	                sourceUrl: data.url
-	            };
-	            callback(gif); 
-	        }
-	    };
-	    xhr.send();
+ 	getGif: function(searchingText) { 
+ 		return new Promise(
+ 			function(resolve, reject) {
+			    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  // 2.
+			    var xhr = new XMLHttpRequest(); 
+			    xhr.open('GET', url);
+			    xhr.onload = function() {
+			        if (xhr.status === 200) {
+						var data = JSON.parse(xhr.responseText).data; 
+			            var gif = {  
+			                url: data.fixed_width_downsampled_url,
+			                sourceUrl: data.url
+			            };
+			            resolve(gif); 
+			        }else {
+			        	reject(new Error(this.statusText));
+			        }
+			    };
+			    xhr.send();
+		});
 	},
 
 	render: function() {
